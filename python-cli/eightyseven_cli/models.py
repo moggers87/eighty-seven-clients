f496c02759edc331bf6ef5f8ac23b629a163469e
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-07-17 21:04:12 +0100

    Changed models, still need to write manager and requests.Session subclass

--- python-cli/eightyseven_cli/models.py
+++ python-cli/eightyseven_cli/models.py
@@ -17,43 +17,96 @@
 
 from __future__ import unicode_literals, print_function
 
-import requests
+import json
 
 class Model(object):
+    """Base model for API objects
+
+    Fields are accessible as attributes"""
     id = None
-    api_path = "/api/v1/"
 
-    def __init__(self, host):
-        self.host = host
+    def __init__(self, session):
+        """Create a blank object
 
-    @property
-    def url(self):
+        `session` should be a subclass of requests.Session that prefixes all
+        URLs with the API root
+        """
+        self._session = session
+
+        # where fields are really stored :D
+        self._fields = {}
+
+    def __getattr__(self, field):
+        if field in self._fields:
+            return self._fields[field]
+        elif self.id is None:
+            raise AttributeError("%s could not be found" % field)
+        else:
+            raise AttributeError("%s cannot be found on object id %s" % (field, self.id))
+
+    def _url(self):
+        """Get URL of object or of object type if it doesn't have an ID """
         name = type(self).__name__.lower()
-        url = "{host}{api_path}{name}/".format(
-                    host=self.host,
-                    api_path=self.api_path,
-                    name=name,
-                    )
+        url = "/{name}/".format(name=name)
         if self.id is not None:
             url = "{url}{id}/".format(url=url, id=self.id)
 
         return url
 
-    def save(self):
-        """Save object to the server"""
-        api_method = getattr(requests, "put")
-        if self.id is None:
-            api_method = getattr(requests, "post")
+    def _prepare_data(self, update_fields=None):
+        """Prepare data and spit out JSON"""
+        if update_fields is None:
+            data = self._fields
+        else:
+            data = {}
+            for field in update_fields:
+                data[field] = self._fields[field]
 
-        try:
-            response = api_method(self.url)
-        except requests.exceptions.ConnectionError:
-            raise Exception("Connection error")
+        return json.dumps(data)
+
+    @classmethod
+    def create(cls, session, data):
+        """Creates object from API data
+
+        It expects a single object as JSON or a dict"""
+        obj = cls(session)
+
+        if not isinstance(data, dict):
+            data = json.loads(data)
+        obj.id = data["id"]
+
+        # we've either dealth with these fields or don't care about them
+        del data["id"]
+        del data["resource_uri"]
+
+        for field in data:
+            obj._fields[field] = data[field]
+
+        return obj
+
+    def save(self, update_fields=None):
+        """Save object to the server"""
+        # prepare data
+        data = self._prepare_data(update_fields)
+
+        # select HTTP method to use
+        api_method = getattr(self._session, "patch")
+        if self.id is None:
+            api_method = getattr(self._session, "post")
+
+        # exceptions will filter up
+        response = api_method(self._url(), data=data)
 
         if response.status_code > 499:
             raise Exception("Server error: %s" % response.status_code)
         elif response.status_code > 399:
             raise Exception("URL not found")
+
+        # work out ID
+        if self.id is None:
+            location = response.headers["location"]
+            location = location.replace(self._url(), "").strip("/")
+            self.id = location
 
 class PasswordStore(Model):
     pass
