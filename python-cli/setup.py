7c1c065f870bb7a589d4a106b1bcd5b198141a89
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-07-17 13:02:09 +0100

    We're GPLv3+

--- python-cli/setup.py
+++ python-cli/setup.py
@@ -8,13 +8,17 @@ config = {
     "author_email": "moggers87+git@moggers87.co.uk",
     "classifiers": [
         "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
     ],
     "description": "A simple client for the EightySeven",
     "long_description": "Simple client for the EightySeven service",
     "name": "eightyseven_cli",
     "py_modules": ["eightyseven_cli"],
-    "scripts": ["bin/eightyseven"],
+    "license": "GPLv3+"
     "version": "0",
+    "entry_points":  {
+        'console_scripts': ['eightyseven=eighyseven_cli:main'],
+    }
 }
 
 setup(**config)
