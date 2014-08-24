367aba77f07c5138e9dde8acf5ce88c88bd73829
Matt Molyneaux <moggers87+git@moggers87.co.uk>
2014-08-24 19:51:06 +0100

    Create Session subclass that prepends all urls

--- python-cli/eightyseven_cli/__init__.py
+++ python-cli/eightyseven_cli/__init__.py
@@ -19,7 +19,57 @@ from __future__ import unicode_literals, print_function
 
 from eightyseven_cli.config import JSONStore
 
+from requests.auth import HTTPDigestAuth
 import requests
+
+class Session(requests.Session):
+    def __init__(self, url):
+        self._prepend_url = url
+        super(Session, self).__init__()
+
+    def prepare_request(self, request):
+        request.url = self._prepend_url + request.url
+        return super(Session, self).prepare_request(request)
+
+    def request(self, method, url, **kwargs):
+        url = self._prepend_url + url
+        return super(Session, self).request(self, method, url, **kwargs)
+
+    def get(self, url, **kwargs):
+        url = self._prepend_url + url
+        return super(Session, self).get(self, url, **kwargs)
+
+    def options(self, url, **kwargs):
+        url = self._prepend_url + url
+        return super(Session, self).options(self, url, **kwargs)
+
+    def head(self, url, **kwargs):
+        url = self._prepend_url + url
+        return super(Session, self).head(self, url, **kwargs)
+
+    def post(self, url, **kwargs):
+        url = self._prepend_url + url
+        return super(Session, self).post(self, url, **kwargs)
+
+    def put(self, url, **kwargs):
+        url = self._prepend_url + url
+        return super(Session, self).put(self, url, **kwargs)
+
+    def patch(self, url, **kwargs):
+        url = self._prepend_url + url
+        return super(Session, self).patch(self, url, **kwargs)
+
+    def delete(self, url, **kwargs):
+        url = self._prepend_url + url
+        return super(Session, self).delete(self, url, **kwargs)
+
+    def send(self, request, **kwargs):
+        request.url = self._prepend_url + request.url
+        return super(Session, self).send(request, **kwargs)
+
+    def get_adapter(self, url):
+        url = self._prepended_url + url
+        return super(Session, self).get_adapter(url)
 
 def main():
     print("Hello")
